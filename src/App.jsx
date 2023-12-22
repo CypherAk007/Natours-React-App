8d8c9ecbc761abaf6297bfb4f71d234279de3c4b
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-22 23:14:16 +0530

    Tours Section copy and pasted

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./components/Header";
 import classes from './App.module.scss'
 import MainSection from "./components/MainSection";
 import Feature from "./components/Feature";
+import Tours from "./components/Tours";
 const App = ()=>{
   return (
     <div className={`${classes.app} p-12`}>
         <Header></Header>
         <MainSection></MainSection>
         <Feature></Feature>
+        <Tours></Tours>
     </div>
   )
 }
---
+++ src/components/Tours.jsx
@@ -0,0 +1,117 @@
+import Button from '../UI/Button';
+import './Tours.scss';
+
+const Tours = () => {
+    return (
+        <section class="section-tour">
+            <div className="u-center-text u-margin-bottom-big flex">
+                <h2 class="heading-secondary">
+                    Most Popular Tours
+                </h2>
+            </div>
+            <div class="section-tour__cards">
+
+                <div class="card">
+                    <div class="card__side card__side--front">
+                        <div class="card__picture card__picture--1">
+                            &nbsp;
+                        </div>
+                        <h4 class="card__heading">
+                            <span class="card__heading-span card__heading-span--1">
+                                The Sea Explorer
+                            </span>
+
+                        </h4>
+                        <div class="card__details">
+                            <ul>
+                                <li>3 day tours</li>
+                                <li>Up to 30 people</li>
+                                <li>2tour guides</li>
+                                <li>Sleep in cozy hotels</li>
+                                <li>Difficulty:easy</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card__side card__side--back card__side--back-1">
+                        <div class="card__cta">
+                            <div class="card__price-box">
+                                <div class="card__price-only">Only</div>
+                                <div class="card__price-value">$297</div>
+                            </div>
+                            <Button>Book now</Button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card__side card__side--front">
+                        <div class="card__picture card__picture--2">
+                            &nbsp;
+                        </div>
+                        <h4 class="card__heading">
+                            <span class="card__heading-span card__heading-span--2">
+                                The Forest Hiker
+                            </span>
+
+                        </h4>
+                        <div class="card__details">
+                            <ul>
+                                <li>7 day tours</li>
+                                <li>Up to 30 people</li>
+                                <li>2tour guides</li>
+                                <li>Sleep in cozy hotels</li>
+                                <li>Difficulty:easy</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card__side card__side--back card__side--back-2">
+                        <div class="card__cta">
+                            <div class="card__price-box">
+                                <div class="card__price-only">Only</div>
+                                <div class="card__price-value">$297</div>
+                            </div>
+                            <Button>Book now</Button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card__side card__side--front">
+                        <div class="card__picture card__picture--3">
+                            &nbsp;
+                        </div>
+                        <h4 class="card__heading">
+                            <span class="card__heading-span card__heading-span--3">
+                                The Snow adventurous
+                            </span>
+
+                        </h4>
+                        <div class="card__details">
+                            <ul>
+                                <li>5day tours</li>
+                                <li>Up to 15 people</li>
+                                <li>6tour guides</li>
+                                <li>Sleep in cozy hotels</li>
+                                <li>Difficulty:Hard</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card__side card__side--back card__side--back-3">
+                        <div class="card__cta">
+                            <div class="card__price-box">
+                                <div class="card__price-only">Only</div>
+                                <div class="card__price-value">$897</div>
+                            </div>
+                            <Button>Book now</Button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className={`flex `}>
+
+            <Button className={`-bg--color-primary my-36 mx-auto text-white after:-bg--color-primary-light`}>Discover all tours</Button>
+            </div>
+        </section>
+    )
+}
+
+export default Tours;
