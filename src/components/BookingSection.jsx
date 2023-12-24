14970c9b9c6352fecb5e6308f397ed51d6b8b58e
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-24 22:17:29 +0530

    form input focus and no focus +invalid border change

--- src/components/BookingSection.jsx
+++ src/components/BookingSection.jsx
@@ -4,7 +4,21 @@ import classes from './BookingSection.module.scss'
 const BookingSection = ()=>{
     return(
         <div className={`${classes['section-book']} `}>
-            <div className={`${classes['book']}`}></div>
+            <div className={`${classes['book']}`}>
+                <div className={`${classes['book__form']}`}>
+                    <form action="#" className={`${classes['form']}`}>
+                    <div className={`pb-[3rem] text-5xl inline-block bg-gradient-to-r -from--color-primary-light -to--color-primary-dark bg-clip-text text-transparent font-semibold uppercase`}>start booking now</div>
+                        <div className={`${classes['form__group']}`}>
+                            <input type='text' className={`${classes['form__input']}`} placeholder='Full Name' id="name" required></input>
+                            <label for='name' className={`${classes['form__label']}`}>Full Name</label>
+                        </div>
+                        <div className={`${classes['form__group']}`}>
+                            <input type='email' className={`${classes['form__input']}`} placeholder='Email Address' id="email" required></input>
+                            <label for='email' className={`${classes['form__label']}`}>Email Address</label>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 
