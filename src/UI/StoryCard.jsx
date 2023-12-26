67be0509c32a51ab0b13be11aa85df250b7fe01b
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-26 08:13:40 +0530

    Media querry basics

--- src/UI/StoryCard.jsx
+++ src/UI/StoryCard.jsx
@@ -6,7 +6,7 @@ const StoryCard = (props)=>{
         <div className={`${classes['story']} relative mt-32 w-[75%] p-[6rem] pl-[9rem] -skew-x-12 bg-white`}>
             <figure className={`${classes['story-shape']}   h-[15rem] w-[15rem]  float-left`}>
                 <img src={props.reviewerpic} className={`h-[100%] `}alt="reviewerpic" />
-                <figcaption className={`text-white block text-[1.7rem] uppercase absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-center opacity-100 hover:opacity-0`}>Aldo</figcaption>
+                <figcaption className={`text-white block text-[1.7rem] uppercase absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-center opacity-100 hover:opacity-0`}>{props.plpname}</figcaption>
             </figure>
             <div className={`${classes['story-text']} skew-x-12`}>
                 <h3 className={`uppercase text-3xl font-semibold`}>{props.heading}</h3>
--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -5,7 +5,7 @@ const Feature = ()=>{
     return (
         <section className={`${classes['section-features']} -skew-y-6 -mt-[10rem]`}>
             {/* <div>image</div> */}
-            <div className={`${classes['feature-row']} mx-[10rem] flex  gap-[8rem] skew-y-6`}>
+            <div className={`${classes['feature-row']} mx-[10rem] flex flex-wrap gap-[8rem] skew-y-6`}>
                 <Card img={'../../public/sprite.svg#icon-dribbble'} heading={'Explore The World'} p={'The connection between the physical world surrounding us and the life inside us. Nature is God\'s'}></Card>
                 <Card img={'../../public/sprite.svg#icon-compass2'} heading={'Explore The World'} p={'The connection between the physical world surrounding us and the life inside us. Nature is God\'s'}></Card>
                 <Card img={'../../public/sprite.svg#icon-map2'} heading={'Explore The World'} p={'The connection between the physical world surrounding us and the life inside us. Nature is God\'s'}></Card>
